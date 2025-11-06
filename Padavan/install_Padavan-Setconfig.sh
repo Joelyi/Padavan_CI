968e7ac08f419bcf0811c94688bbae77f157052f
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-11-06 14:38:29 +0800

    Update install_Padavan-Setconfig.sh

--- Padavan/install_Padavan-Setconfig.sh
+++ Padavan/install_Padavan-Setconfig.sh
@@ -11,5 +11,5 @@ cp -f ${ROOTDIR}/ttyd/Makefile $default_path/libs/libssl/Makefile
 cp -f ${ROOTDIR}/libssl/Makefile $default_path/libs/libssl/Makefile
 cp -f ${ROOTDIR}/libcurl/Makefile $default_path/libs/libcurl/Makefile
 sudo rm -rf $default_path/user/ipset
-cp -rf ${ROOTDIR}/ipset/. $default_path/user/ipset
+cp -rf ${ROOTDIR}/ipset $default_path/user
 	      echo "--------------更改Padavan文件结束------------------"
