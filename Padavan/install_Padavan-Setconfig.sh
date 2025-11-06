520d17b62c2ed2c52e187ac2dc246364f1581367
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-11-06 15:24:22 +0800

    Update install_Padavan-Setconfig.sh

--- Padavan/install_Padavan-Setconfig.sh
+++ Padavan/install_Padavan-Setconfig.sh
@@ -6,12 +6,10 @@ ROOTDIR=`pwd`
 default_path="${DESTDIR}/trunk"
 
 echo "--------------开始复制配置文件----------------------"
-sudo rm -rf $default_path/user/ttyd/ttyd-1.7.4.tar.gz
-cp -f ${ROOTDIR}/ttyd/Makefile $default_path/libs/libssl/Makefile
+cp -f ${ROOTDIR}/ttyd/Makefile $default_path/user/ttyd/Makefile
 cp -f ${ROOTDIR}/libssl/Makefile $default_path/libs/libssl/Makefile
 cp -f ${ROOTDIR}/libcurl/Makefile $default_path/libs/libcurl/Makefile
 sudo rm -rf $default_path/user/ipset
 cp -rf ${ROOTDIR}/ipset $default_path/user
-cd $default_path/user/ipset/ipset-7.11
-chmod +x autogen.sh
+sudo chmod 777 ./ipset
 	      echo "--------------更改Padavan文件结束------------------"
